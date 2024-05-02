08f4ce322ab72cdcbe1225145fc275dffb17b2d2
James Pike <jamesandrewpike@gmail.com>
2024-05-02 00:30:05 -0400

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -12,14 +12,121 @@ export default component$(() => {
       <div class="absolute inset-0 pointer-events-none" aria-hidden="true"></div>
 
       
-      <div class="relative max-w-7xl mx-auto px-4">
+      <div class="relative max-w-7xl mx-auto md:px-4">
 
         
         <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
-        <video width="1400" height="240"  poster="./images/a6.png" controls src="./images/almonte1.mp4">
+        {/* <video width="320" height="240"  controls src="./images/almonte1.mp4">
   Your browser does not support the video tag.
-</video>
-      
+</video> */}
+
+<div style={{ position: 'relative', height: '50vh' }}>
+<Image
+     className="object-cover w-full "
+  src="/images/g1.jpg"
+  alt="{name}"
+  quality={50}
+  width={0}
+  height={0}
+  style={{ width: 'full', height: '50vh', opacity: 0.75 }} // optional
+  priority
+/>
+                        <h1
+                            style={{
+                                position: 'absolute',
+                                top: '40%', // Position the h1 in the middle vertically
+                                left: '20%', // Position the h1 in the middle horizontally
+                                transform: 'translate(-20%, -50%)', // Center the h1
+                                color: 'white', // Text color
+                                fontSize: '2.4rem', // Font size
+                                zIndex: 10, // Ensure it's above the image
+                                lineHeight: '1', // Increase line height for better spacing between lines
+                            }}
+                        >
+                            <span style={{ display: 'block' }}>CANADAS</span>
+                            <span style={{ display: 'block' }}>CERTIFIED</span>
+                            <span style={{ display: 'block' }}>PINSARIA</span>
+                        </h1>
+                        <div className="pt-6"
+                            style={{
+                                position: 'absolute',
+                                top: '60%', // Position the buttons below the subtitle
+                                left: '20%', // Position the buttons in the middle horizontally
+                                transform: 'translateX(-20%)', // Center the buttons horizontally
+                                zIndex: 10, // Ensure it's above the image
+                            }}
+                        >
+                            <div className="pt-1" style={{ display: 'flex', alignItems: 'center' }}>
+                                <a href="/menu">
+                                    <button className="px-4 py-3 mr-2 text-xs font-bold text-white bg-black border-white rounded border-1">
+                                        BROWSE MENU
+                                    </button>
+                                </a>
+                                <div className="pt-2"></div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div className="border-black border-6"></div>
+
+
+
+                    <div style={{ position: 'relative', height: '30vh' }}>
+<Image
+     className="object-cover w-full "
+  src="/images/p1.jpg"
+  alt="{name}"
+  quality={50}
+  width={440}
+  height={0}
+  style={{ width: 'full', height: '30vh', opacity: 0.75 }} // optional
+  priority
+/>
+                        <h1
+                            style={{
+                                position: 'absolute',
+                                top: '40%', // Position the h1 in the middle vertically
+                                left: '20%', // Position the h1 in the middle horizontally
+                                transform: 'translate(-20%, -50%)', // Center the h1
+                                color: 'white', // Text color
+                                fontSize: '2.4rem', // Font size
+                                zIndex: 10, // Ensure it's above the image
+                                lineHeight: '1', // Increase line height for better spacing between lines
+                            }}
+                        >
+                            <span style={{ display: 'block' }}>CANADAS</span>
+                            <span style={{ display: 'block' }}>CERTIFIED</span>
+                            <span style={{ display: 'block' }}>PINSARIA</span>
+                        </h1>
+                        <div className="pt-6"
+                            style={{
+                                position: 'absolute',
+                                top: '60%', // Position the buttons below the subtitle
+                                left: '20%', // Position the buttons in the middle horizontally
+                                transform: 'translateX(-20%)', // Center the buttons horizontally
+                                zIndex: 10, // Ensure it's above the image
+                            }}
+                        >
+                            <div className="pt-1" style={{ display: 'flex', alignItems: 'center' }}>
+                                <a href="/menu">
+                                    <button className="px-4 py-3 mr-2 text-xs font-bold text-white bg-black border-white rounded border-1">
+                                        BROWSE PINSAS
+                                    </button>
+                                </a>
+                                <div className="pt-2"></div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div className="border-8 border-black"></div>
+
+
         <div class="py-12 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
           <div class="basis-1/2 text-center lg:text-left pb-10 md:pb-16 mx-auto">
             <h1 class="text-5xl md:text-6xl font-bold leading-tighter tracking-tighter mb-4 font-heading dark:text-gray-200">
